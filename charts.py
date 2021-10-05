0e31128daff2125b516459ce480fcd3f2713b75c
jeff <jeffli.1644@gmail.com>
2021-10-05 16:19:22 -0400

    dual axis plot

---
+++ charts.py
@@ -0,0 +1,28 @@
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# fake data
+scores = [10, 30, 100, 40, 50]
+total_score = sum(scores)
+scores_pct = [x / total_score for x in scores]
+players = ["Alice", "Bob", "Carmen", "Dickson", "Earl"]
+x = np.arange(len(players))
+width = 0.35  # width of bars
+
+# %%
+fig, ax = plt.subplots()
+
+# normal bar plot
+ax.bar(x - width / 2, scores_pct, width, label="Players")
+ax.set_xticks(x)
+ax.set_xticklabels(players)
+pct_ticks = ["{:.2%}".format(x) for x in ax.get_yticks()]
+ax.set_yticklabels(pct_ticks)
+
+
+ax2 = ax.twinx()
+ax2.set_yticklabels([0] + sorted(scores))
+
+# %%
